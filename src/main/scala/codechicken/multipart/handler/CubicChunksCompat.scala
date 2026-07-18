846f104e3254c81b3c6fbdf0430620bd2d7beda3
RecursivePineapple <recursive_pineapple@proton.me>
2026-07-18 13:08:14 -0400

    Fix TE interceptor for CC

---
+++ src/main/scala/codechicken/multipart/handler/CubicChunksCompat.scala
@@ -0,0 +1,28 @@
+package codechicken.multipart.handler
+
+import com.cardinalstar.cubicchunks.api.event.CubeEvent
+import cpw.mods.fml.common.eventhandler.{EventPriority, SubscribeEvent}
+import net.minecraftforge.common.MinecraftForge
+import scala.collection.JavaConversions._
+
+object CubicChunksCompat {
+  def init(): Unit = MinecraftForge.EVENT_BUS.register(this)
+
+  @SubscribeEvent(priority = EventPriority.HIGHEST)
+  def onTileEntitiesLoad(event: CubeEvent.TileEntitiesLoad): Unit = {
+    val cube = event.cube
+    val world = event.world
+
+    val iter = cube.cubeTileEntityMap.entrySet().iterator()
+
+    while (iter.hasNext) {
+      val e = iter.next();
+
+      val converted = MultipartSaveLoad.convertTileForCube(world, e.getValue)
+      if (converted != e.getValue) {
+        if (converted != null) e.setValue(converted)
+        else cube.cubeTileEntityMap.remove(e.getKey)
+      }
+    }
+  }
+}
--- src/main/scala/codechicken/multipart/handler/MultipartSaveLoad.scala
+++ src/main/scala/codechicken/multipart/handler/MultipartSaveLoad.scala
@@ -58,6 +58,34 @@ object MultipartSaveLoad {
     field.get(null).asInstanceOf[Map[Class[_ <: TileEntity], String]]
   }
 
+  def convertTileForCube(world: World, tile: TileEntity): TileEntity = {
+    loadingWorld = world
+    tile match {
+      case s: TileNBTContainer if s.tag.getString("id") == "savedMultipart" =>
+        val newTile = TileMultipart.createFromNBT(s.tag)
+        if (newTile != null) {
+          newTile.setWorldObj(world)
+          newTile.validate()
+        }
+        newTile
+      case t =>
+        converters.find(_.canConvert(t)) match {
+          case Some(c) =>
+            val parts = c.convert(t)
+            if (parts.isEmpty) null
+            else {
+              val newTile = MultipartHelper.createTileFromParts(parts)
+              if (newTile != null) {
+                newTile.setWorldObj(world)
+                newTile.validate()
+              }
+              newTile
+            }
+          case None => tile
+        }
+    }
+  }
+
   def loadTiles(chunk: Chunk) {
     loadingWorld = chunk.worldObj
     val iterator = chunk.chunkTileEntityMap
